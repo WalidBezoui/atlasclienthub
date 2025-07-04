8d0de1ecf02e0fc11a393f1b661be8065e47c726
bezoui walid <walid.bezoui@etud.iga.ac.ma>
2025-07-04 22:03:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -24,7 +23,7 @@ AccordionItem.displayName = "AccordionItem"
 const AccordionTrigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+>(({ className, children, asChild = false, ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       ref={ref}
@@ -32,11 +31,11 @@ const AccordionTrigger = React.forwardRef<
         "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
+      asChild={asChild}
       {...props}
     >
       {children}
-      {/* Do not render chevron if using asChild to avoid multiple children error */}
-      {!(props as { asChild?: boolean }).asChild && (
+      {!asChild && (
         <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
       )}
     </AccordionPrimitive.Trigger>
